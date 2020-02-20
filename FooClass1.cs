a02fd170927df0cb27012399f397c3268b4245c1
Chad Ziesemer <CZiesemer@faith1.net>
2020-02-20 11:06:05 -0600

    Add project files.

---
+++ FooClass1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UWPUnitTestIssue
+{
+    public class FooClass1
+    {
+        public int Foo1 { get; set; }
+
+    }
+}
